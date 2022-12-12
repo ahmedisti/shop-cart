4fe50a894be317b485ab514245e652e441f38902
ahmedisti <mdistiaqah@gmail.com>
2022-12-12 15:26:45 +0600

    cart data store in localstorage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { addToDb, getStoredCard } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
@@ -9,14 +10,52 @@ const Shop = () => {
 
 
     useEffect(() => {
+      // console.log('product load first before fetch');
         fetch('products.json')
         .then(res => res.json())
-        .then(data => setProducts(data))
+        .then(data => {
+          setProducts(data)
+          // console.log('products loaded');
+        })
     },[]);
-    const handleAddToCart = (product) => {
-        // console.log(product);
-        const newCart = [...cart , product];
+
+    // use data in the local storage thats why we are using this useEffect and we are import getStoredCart method form fakebd.js
+    useEffect( () => {
+      console.log('local storage first line');
+      const storedCart = getStoredCard();
+      const saveCart = [];
+      for(const id in storedCart){
+        const addedProduct = products.find(product => product.id === id);
+        if(addedProduct){
+
+          const quantity = storedCart[id];
+          addedProduct.quantity = quantity;
+          saveCart.push(addedProduct);
+        }
+        
+
+      }
+
+      setCart(saveCart);
+      // console.log('local storage finished');
+      
+    } , [products])
+    const handleAddToCart = (seletedProduct) => {
+        console.log(seletedProduct);
+        let newCart = []
+        const exists = cart.find(product => product.id === seletedProduct.id);
+        if(!exists){
+          seletedProduct.quantity = 1;
+          newCart = [...cart,seletedProduct];
+        }
+        else{
+          const rest = cart.filter(product => product.id !== seletedProduct.id);
+          exists.quantity = exists.quantity + 1;
+          newCart = [...rest,exists]
+        }
+        
         setCart(newCart);
+        addToDb(seletedProduct.id)
             }   
 
   return (
